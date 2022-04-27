9eda3940eabe61af0b6aa9abf81fcdf732153aa4
nogataka <nogataka@gmail.com>
2022-04-27 22:59:13 +0900

    ok

--- pages/[[...path]].js
+++ pages/[[...path]].js
@@ -1,59 +1,145 @@
+import merge from 'deepmerge'
+import { DruxtClient } from 'druxt'
+import { DruxtRouter } from 'druxt-router'
+import { DruxtSchema } from 'druxt-schema'
+import { useEffect } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import Head from 'next/head'
+import CommonMeta from '../components/CommonMeta'
+import marked from 'marked';
 
-/*
-export async function getServerSideProps() {
-  return { props: { "data":"TEST" } }
-}
-*/
+//const baseUrl = 'https://demo-api.druxtjs.org'
+//const baseUrl = 'http://192.168.1.77/drupal9/'
+const baseUrl = 'https://panatech.c1x.biz/drupal9/'
 
-export default function Home() {
+function Route(ctx) {
+  if(!ctx) {
+    return (<div>NULL OBJ!</div>)
+  }
+  const router = useRouter()
+  if (ctx.redirect) {
+    useEffect(() => router.push(ctx.redirect))
+    return (<p>Redirecting...</p>)
+  }
+  switch ((ctx.route || {}).type) {
+    case 'entity': {
+      const { entity, schema } = ctx
+      const { attributes, relationships } = entity.data
 
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      return (<>
+      <CommonMeta title="About" description="This is About page." />
+        <div>
+        <p><Link href={'/home'}>HOME</Link></p>
+        <div className="container-fluid">
+          <h1>{attributes.title}</h1>
+          <div className="row row-eq-height">
+            <div className="card-base col-lg-3 col-sm-6 p-1">
+            <div className="card">
+            <div className="card-body">
+            <div dangerouslySetInnerHTML={attributes.body && { __html: marked(attributes.body.value)}} />
+            </div>
+            </div>
+            </div>
+          </div></div></div>
+      </>)
+    }
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    case 'views': {
+      const { displayId, results, view } = ctx
+      const { attributes } = view.data
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+      const display = displayId === 'default'
+        ? attributes.display[displayId]
+        : merge(attributes.display.default, attributes.display[displayId])
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+      return (<>
+      <CommonMeta title="About" description="This is About page." />
+        <div className="container-fluid">
+          <h1>{attributes.label}</h1>
+          <div className="row row-eq-height">
+            {results.data.map(entity => (
+            <div key={entity.id} className="card-base col-lg-3 col-sm-6 p-1">
+            <div className="card">
+            <div className="card-body">
+            <p>{entity.attributes.title}</p>
+            <p>{entity.type}</p>
+            <p>{entity.id}</p>
+            </div>
+            <div className="card-footer">
+            <Link href={'/drupal9'+entity.attributes.path.alias}>{entity.attributes.path.alias}</Link>
+            </div>
+            </div>
+            </div>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            ))}
+            </div>
+            <dt>{/*Display options*/}</dt>
+            <dd>{/*JSON.stringify(display)*/}</dd>
         </div>
-      </main>
-    </div>
-  )
+        </>)
+    }
+  }
+
+  return (<div>Error: Nothing here!</div>)
 }
+
+export async function getServerSideProps({ query, res }) {
+  console.log("query2："+JSON.stringify(query))
+  const token = 'cGFuYXNvbmljOnByb2N0b3Jz'
+  
+  const router = new DruxtRouter(baseUrl,{
+      axios: {
+        headers: {'X-Custom-Header': true,'Authorization': `Basic ${token}`},
+      },
+      endpoint: 'jsonapi'
+  })
+  const path = ((query || {}).path || []).join('/')
+  const { redirect, route } = await router.get(`/${path}`)
+
+  //console.info(`/${path}`)
+  console.log(`/${path}`)
+  if (redirect) {
+    return { props: { redirect } }
+  }
+
+  const druxt = new DruxtClient(baseUrl,{
+    axios: {
+      headers: {'X-Custom-Header': true,'Authorization': `Basic ${token}`},
+    },
+    endpoint: 'jsonapi'
+  })
+  const druxtSchema = new DruxtSchema(baseUrl,{
+    axios: {
+      headers: {'X-Custom-Header': true,'Authorization': `Basic ${token}`},
+    },
+    endpoint: 'jsonapi'
+  })
+
+  switch (route.type) {
+    case 'entity': {
+      const { type, uuid } = route.props
+      const [entityType, bundle] = type.split('--')
+
+      const [entity, { schema }] = await Promise.all([
+        druxt.getResource(type, uuid),
+        druxtSchema.getSchema({ entityType, bundle })
+      ])
+
+      return { props: { entity, route, schema }}
+    }
+
+    case 'views': {
+      const { displayId, type, uuid, viewId } = route.props
+      const [view, results] = await Promise.all([
+        druxt.getResource(type, uuid),
+        druxt.getResource(`views--${viewId}`, displayId)
+      ])
+      return { props: { displayId, view, results, route } }
+    }
+  }
+  console.info('OKOKOK')
+  return { props: { route } }
+}
+
+export default Route
