b321fa6cd3239b7235e33c6a30c6e6befb70a345
nogataka <nogataka@gmail.com>
2022-04-28 09:12:44 +0900

    ok

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps, router }) {
-  console.log("DEBUG");
+  console.log("DEBUG:"+JSON.stringify(pageProps)+" ***** "+router.pathname);
   useEffect(() => {
 	  console.log("OKOK");
   },[router.pathname])
