1a88ef3e82577e7772b1a833fc3c12be78999dbd
nogataka <nogataka@gmail.com>
2022-04-28 09:04:48 +0900

    ok

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps, router }) {
+  console.log("DEBUG");
   useEffect(() => {
 	  console.log("OKOK");
   },[router.pathname])
+
   useEffect(() => {
     console.log("初回だけ走るよ〜");
   }, []);
